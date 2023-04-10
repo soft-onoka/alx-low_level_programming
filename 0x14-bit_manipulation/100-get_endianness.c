b3bf2fbb20a0117920f5101e14702d060a32d710
onoka416 <onokabilly@gmail.com>
2023-04-10 12:37:59 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+/**
+ * get_endianness - checks the endianness of the machine
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
+

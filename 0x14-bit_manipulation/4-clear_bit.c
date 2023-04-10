14187e716345c6d1842f63f4512ea37b13f7124d
onoka416 <onokabilly@gmail.com>
2023-04-10 12:12:18 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,15 +9,14 @@
  *  Return: 1 (success) else -1
     */
 int clear_bit(unsigned long int *n, unsigned int index)
-    {
-        if (index > 64)
-                   return (-1);
-
-
-        if ((*n >> index) & 1)
-     {
-             *n -= 1 << index;
-             return (1);
-     }
-     return (1);
+{
+	if (index > 64)
+		return (-1);
+	if ((*n >> index) & 1)
+	{
+		*n -= 1 << index;
+		return (1);
+	}
+	return (1);
 }
+

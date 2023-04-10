ac27823944e13a8f510055d320241d00e542598b
onoka416 <onokabilly@gmail.com>
2023-04-10 13:28:29 -0700

    -get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,23 +3,21 @@
 #include <stdio.h>
 
 /**
+ * get_bit - function that returns the value of a bit at a given index
+ *
+ * @n: number to scan
+ * @index: bit to return
+ *
+ * Return: Bit (0 | 1)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
 
-    get_bit - function that returns the value of a bit at a given index
+	if (index > 64)
+		return (-1);
 
-    @n: number to scan
-
-    @index: bit to return
-
-    Return: Bit (0 | 1)
-    */
-    int get_bit(unsigned long int n, unsigned int index)
-    {
-    int bit;
-
-     if (index > 64)
-             return (-1);
-
-     bit = ((n >> index) & 1);
-     return (bit);
-
+	bit = ((n >> index) & 1);
+	return (bit);
 }
+

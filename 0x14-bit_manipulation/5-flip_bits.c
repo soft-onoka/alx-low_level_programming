68d0d8a68984fc3e3dccb2f8594859e357112caf
onoka416 <onokabilly@gmail.com>
2023-04-10 13:07:48 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,21 +6,19 @@
  *
  * Return: number bits flipped
  */
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-        unsigned int bits = 0;
-        unsigned long int xor_val;
+	unsigned int bits = 0;
+	unsigned long int xor_val;
 
-        xor_val = n ^ m;
+	xor_val = n^m;
 
-        while (xor_val)
-        {  
-                bits += xor_val & 1;
-                xor_val >>= 1;
-        } 
+	while (xor_val)
+	{
+		bits += xor_val & 1;
+		xor_val >>= 1;
+	}
 
-         return (bits);
+	return (bits);
 }
-
 
